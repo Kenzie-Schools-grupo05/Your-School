7a034569287f01c1845e45db8eaaf58754846713
AlexandreVianaDev <alexandreviana94@hotmail.com>
2023-03-07 13:01:00 -0300

    feat: dashboard with switch case

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,17 +2,25 @@ import React, { useContext } from "react";
 import Header from "../../components/Header";
 import { UserContext } from "../../Providers/UserContext";
 import { DashboardParent } from "../DashboardParent";
+import DashboardProfessor from "../DashboardTeacher"
 
 export const Dashboard = () => {
   const { user } = useContext(UserContext);
 
+  //Switch case para definir o que vai ser renderizado
+  const renderSwitch = (type : string | undefined) => {
+    switch(type) {
+      // case "student": return <DashboardStudent />;
+      case "parent": return <DashboardParent />;
+      case "professor": return <DashboardProfessor />;
+    }
+  }
+
   return (
     <>
       <Header />
-      {/* {user?.type === "student" ? <DashboardStudent /> : null} */}
-      {/* {user?.type === "parent" ? <DashboardParent /> : null} */}
+      {/* {renderSwitch(user?.type)} */}
       <DashboardParent />
-      {/* {user?.type === "professor" ? <DashboardProfessor /> : null} */}
     </>
   );
 };
