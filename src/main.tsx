31a6aafcf06f96a9ed7b40619bee8fcd0777d02c
GustavoCruz <gustavo0201cruz@gmail.com>
2023-03-13 15:53:20 -0400

    fix imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Providers from "./Providers/Providers";
+import { Providers } from "./Providers/Providers";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
